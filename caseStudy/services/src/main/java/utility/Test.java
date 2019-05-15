dc8fd2cb75148d31711247a5648296806d10361e
Claire Zheng <claire.zheng.0110@gmail.com>
2019-05-15 17:16:59 -0400

    input validated for stock now

--- caseStudy/services/src/main/java/utility/Test.java
+++ caseStudy/services/src/main/java/utility/Test.java
@@ -1,15 +1,67 @@
 package utility;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import pojo.Company;
 
 public class Test {
+//	public static void main(String[] args) {
+//		JSONArray obj = null;
+//		try {
+//			obj = (JSONArray) new JSONParser().parse(new FileReader("companyInfo.json"));
+//		} catch (IOException e) {
+//			System.out.println("Error 1");
+//			return;
+//		} catch (ParseException e) {
+//			System.out.println("Error 2");
+//			return;
+//		}
+//		boolean valid = true;
+//		
+//		for (int i = 0; i < obj.size(); i++) {
+//			JSONObject jobj = (JSONObject) obj.get(i);
+//			
+//			boolean symbolValid = jobj.get("symbol") instanceof String || jobj.get("symbol") == null;
+//			boolean nameValid = jobj.get("name") instanceof String || jobj.get("name") == null;
+//			boolean hqCityValid = jobj.get("headquartersCity") instanceof String || jobj.get("headquartersCity") == null;
+//			boolean hqStateValid = jobj.get("headquartersStateOrCountry") instanceof String || jobj.get("headquartersStateOrCountry") == null;
+//			boolean numEmployeesValid = jobj.get("numberOfEmployees") instanceof Integer && (int) jobj.get("numberOfEmployees") >= 0 || jobj.get("numberOfEmployees") == null;
+//			boolean sectorValid = jobj.get("sector") instanceof String || jobj.get("sector") == null;
+//			boolean industryValid = jobj.get("industry") instanceof String || jobj.get("industry") == null;
+//			
+//			if (symbolValid && nameValid && hqCityValid && hqStateValid && numEmployeesValid && sectorValid && industryValid)
+//				valid = false;
+//			else {
+//				String symbol = symbolValid ? (String) jobj.get("symbol") : null;
+//				String name = nameValid ? (String) jobj.get("name") : null;
+//				String hqCity = hqCityValid ? (String) jobj.get("headquartersCity") : null;
+//				String hqState = hqStateValid ? (String) jobj.get("headquartersStateOrCountry") : null;
+//				int numEmployees = numEmployeesValid ? (int) jobj.get("numberOfEmployees") : -1;
+//				String sector = sectorValid ? (String) jobj.get("sector") : null;
+//				String industry = industryValid ? (String) jobj.get("industry") : null;
+//				
+//				System.out.println("Symbol: " + symbol);
+//				System.out.println("Name: " + name);
+//				System.out.println("HQ City: " + hqCity);
+//				System.out.println("HQ State: " + hqState);
+//				System.out.println("Employees: " + numEmployees);
+//				System.out.println("Sector: " + sector);
+//				System.out.println("Industry: " + industry);
+//				System.out.println();
+//				
+//				
+////				Company comp = new Company(symbol, name, hqCity, hqState, numEmployees, sector, industry);
+//			}
+//		}
+//	}
 	public static void main(String[] args) {
 		JSONArray obj = null;
 		try {
-			obj = (JSONArray) new JSONParser().parse(new FileReader("companyInfo.json"));
+			obj = (JSONArray) new JSONParser().parse(new FileReader("C:\\Users\\clair\\Documents\\EngineeringEssentials\\caseStudy\\services\\src\\main\\java\\utility\\historicalStockData.json"));
 		} catch (IOException e) {
 			System.out.println("Error 1");
 			return;
@@ -22,37 +74,24 @@ public class Test {
 		for (int i = 0; i < obj.size(); i++) {
 			JSONObject jobj = (JSONObject) obj.get(i);
 			
-			boolean symbolValid = jobj.get("symbol") instanceof String || jobj.get("symbol") == null;
-			boolean nameValid = jobj.get("name") instanceof String || jobj.get("name") == null;
-			boolean hqCityValid = jobj.get("headquartersCity") instanceof String || jobj.get("headquartersCity") == null;
-			boolean hqStateValid = jobj.get("headquartersStateOrCountry") instanceof String || jobj.get("headquartersStateOrCountry") == null;
-			boolean numEmployeesValid = jobj.get("numberOfEmployees") instanceof Integer && (int) jobj.get("numberOfEmployees") >= 0 || jobj.get("numberOfEmployees") == null;
-			boolean sectorValid = jobj.get("sector") instanceof String || jobj.get("sector") == null;
-			boolean industryValid = jobj.get("industry") instanceof String || jobj.get("industry") == null;
+			String company = (String) jobj.get("name");
+			JSONArray innerObj = (JSONArray) jobj.get("dailyClosePrice");
+			HashMap<String, Double> vals = new HashMap<String, Double>();
 			
-			if (symbolValid && nameValid && hqCityValid && hqStateValid && numEmployeesValid && sectorValid && industryValid)
-				valid = false;
-			else {
-				String symbol = symbolValid ? (String) jobj.get("symbol") : null;
-				String name = nameValid ? (String) jobj.get("name") : null;
-				String hqCity = hqCityValid ? (String) jobj.get("headquartersCity") : null;
-				String hqState = hqStateValid ? (String) jobj.get("headquartersStateOrCountry") : null;
-				int numEmployees = numEmployeesValid ? (int) jobj.get("numberOfEmployees") : -1;
-				String sector = sectorValid ? (String) jobj.get("sector") : null;
-				String industry = industryValid ? (String) jobj.get("industry") : null;
-				
-				System.out.println("Symbol: " + symbol);
-				System.out.println("Name: " + name);
-				System.out.println("HQ City: " + hqCity);
-				System.out.println("HQ State: " + hqState);
-				System.out.println("Employees: " + numEmployees);
-				System.out.println("Sector: " + sector);
-				System.out.println("Industry: " + industry);
-				System.out.println();
-				
-				
-//				Company comp = new Company(symbol, name, hqCity, hqState, numEmployees, sector, industry);
+			for(Map.Entry<String, Object> entry : ((HashMap<String,Object>) innerObj.toArray()[0]).entrySet()) {
+			    String key = entry.getKey();
+			    
+			    Object value = entry.getValue();
+			    double realValue;
+			    if (value.toString().contains("."))
+			    	realValue = (double) value;
+			    else
+			    	realValue = ((Long) value).doubleValue();
+			    System.out.println("val: " + realValue);
+			    
+			    vals.put(key, realValue);
 			}
+			System.out.println();
 		}
 	}
 }
