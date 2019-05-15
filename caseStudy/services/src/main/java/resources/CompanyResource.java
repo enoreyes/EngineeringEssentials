9f1c20c269fbd80eb2849bcb584dae7a24227817
Eno Reyes <enoreyes@gmail.com>
2019-05-15 15:49:28 -0400

    Fixed CompanyResource

--- caseStudy/services/src/main/java/resources/CompanyResource.java
+++ caseStudy/services/src/main/java/resources/CompanyResource.java
@@ -15,11 +15,55 @@
  */
 
 package resources;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+import pojo.Company;
+import pojo.Stock;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.SortedMap;
+import java.text.ParseException;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
 
 // TODO - add your @Path here
+@Path("company")
 public class CompanyResource {
 
     // TODO - Add a @GET resource to get company data
     // Your service should return data for a given stock ticker
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @GET
+    public Response getByTicker(String ticker) throws IOException {
+        //TODO: Return the list of all of the events in the events.json file
+        List<Companiy> companies; // something Claire makes
+
+        for (Company c : companies) {
+            if (c.getSymbol() == ticker) {
+                return Response.ok().entity(c).build();
+            }
+        }
+        return Response.noContent().build();
+    }
+
 
 }
