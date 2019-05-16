071325a518e85268dc19b1181b4da9a0f713fe8e
kimjuyounglisa <39270614+kimjuyounglisa@users.noreply.github.com>
2019-05-16 02:58:23 -0400

    Update LineChart.js

--- caseStudy/ui/src/components/charts/LineChart.js
+++ caseStudy/ui/src/components/charts/LineChart.js
@@ -1,107 +1,62 @@
 /**
- * Copyright 2019 Goldman Sachs.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+* Copyright 2018 Goldman Sachs.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+**/
 
 import React from 'react';
 import Highcharts from 'highcharts';
 
 class LineChart extends React.Component {
-    constructor(props) {
-        super(props);
-        this.highchartsOptions = {
-            chart: {
-                zoomType: 'x'
-            },
-            title: {
-                text: 'Company Name'
-            },
-            xAxis: {
-                type: 'datetime'
-            },
-            yAxis: {
-                title: {
-                    text: 'Stock value'
-                }
-            },
-            plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
-                    marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
-                }
-            },
-            series: [{
-                name: 'Prices',
-                data: this.props.data
-            }]
+  constructor(props) {
+    super(props);
+    this.highchartsOptions = {
+      title: {
+        text: 'Posts Per Month'
+      },
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      yAxis: {
+        title: {
+          text: 'Number of Posts'
         }
-    }
+      },
+      series: [{
+        name: 'John Smith',
+        marker: {
+          symbol: 'square'
+        },
+        data: props.dataOne
 
-    componentDidMount() {
-        Highcharts.chart('chart', this.highchartsOptions);      
+      }, {
+        name: 'Jane Doe',
+        marker: {
+          symbol: 'diamond'
+        },
+        data: props.dataTwo
+      }]
     }
+  }
 
-    componentWillReceiveProps(props) {
-        console.log("New data received to redraw chart.");
-        const chartData = (props.data).map((props) => {
-            return (
-                <p>{Date.UTC(props)}</p>
-            );
-        });
-        /**
-         * TODO
-         * Parse the data received from props, a Javascript object, to map to a Javascript array
-         * required by the type of line chart chosen and set it in the series. Use Date.UTC(..)
-         * to create the x-axis.
-         */
-        
-        /**
-         * TODO
-         * Uncomment the line below to pass the data be displayed to the series*/
-        this.chart.series[0].setData(chartData);
-    }
+  componentDidMount() {
+    Highcharts.chart('chart', this.highchartsOptions);
+  }
 
-    componentWillUnmount() {
-        this.chart.destroy();
-    }
-
-
-    render() {
-        return (
-            <div id='chart'></div>
-        )
-    }
+  render() {
+    return <div id='chart'></div>
+  }
 }
 
 export default LineChart;
